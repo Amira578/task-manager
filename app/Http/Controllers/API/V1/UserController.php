f87edbff39c8a68fd8d1df93683d9bb6c31dd068
Amira <es-amira.khalid2018@alexu.edu.eg>
2021-01-30 17:22:50 +0200

    [Added] UserController [Added] Password setter in user model for encryption [Added] api routes for list users,show a user, add a user and update a user [updated] UserController by adding index,show,store and update functions

---
+++ app/Http/Controllers/API/V1/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+        return User::all();
+    }
+    public function show( User $user){
+        return $user;
+    }
+    public function store( Request $request){
+       $user= User::create($request->all());
+        return $user;
+    }
+    public function update( Request $request, User $user){
+        $user->update($request->all());
+        return  $user;
+    }
+}
+
