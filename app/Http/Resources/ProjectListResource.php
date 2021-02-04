64f4b558b1285d06b0062662d4499a310d99aa13
Amira <es-amira.khalid2018@alexu.edu.eg>
2021-02-04 22:50:21 +0200

    [Added] ProjectFactory.php [Added] project test cases

--- app/Http/Resources/ProjectListResource.php
+++ app/Http/Resources/ProjectListResource.php
@@ -15,6 +15,7 @@ class ProjectListResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'=>$this->id,
             'name'=>$this->name
         ];
     }
