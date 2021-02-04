19f72ea530df55874dc8dbc1ff42b6f6da5a2157
Amira <es-amira.khalid2018@alexu.edu.eg>
2021-02-04 16:51:29 +0200

    [Created] Project Resources [Created] Project Requests

--- app/Http/Controllers/API/V1/ProjectController.php
+++ app/Http/Controllers/API/V1/ProjectController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProjectNewRequest;
+use App\Http\Requests\ProjectUpdateRequest;
+use App\Http\Resources\ProjectListResource;
+use App\Http\Resources\ProjectShowResource;
 use Illuminate\Http\Request;
 use App\Models\Project;
 
@@ -10,18 +14,18 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        return Project::all();
+        return ProjectListResource::collection( Project::all());
     }
     public function show( Project $project)
     {
-        return $project;
+        return (new ProjectShowResource($project));
     }
-    public function store( Request $request)
+    public function store( ProjectNewRequest $request)
     {
         $project= Project::create($request->all());
         return $project;
     }
-    public function update( Request $request, Project $project)
+    public function update( ProjectUpdateRequest $request, Project $project)
     {
         $project->update($request->all());
         return  $project;
